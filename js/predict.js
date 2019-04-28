ff351277ff327dc55468479cc28b9000f69b0cf1
TheNadhi <thenadi@outlook.com>
2019-04-28 18:20:06 +0530

    _data/js files not loading

---
+++ js/predict.js
@@ -0,0 +1,161 @@
+var predictionByParticipants=`#,Constituency,Name,Winner-Predict,Winner-MarginPredict
+1,Kasaragod,Baiju,K P Satheesh Chandran (CPI(M)),3000
+1,Kasaragod,Ranish,K P Satheesh Chandran (CPI(M)),10000
+1,Kasaragod,Shanavas,K P Satheesh Chandran (CPI(M)),5000
+1,Kasaragod,SKB,Rajamohan Unnithan (INC),1000
+1,Kasaragod,Shibu,Rajamohan Unnithan (INC),10000
+1,Kasaragod,Shiju,K P Satheesh Chandran (CPI(M)),60000
+1,Kasaragod,Becker,K P Satheesh Chandran (CPI(M)),40000
+1,Kasaragod,Swami,K P Satheesh Chandran (CPI(M)),9
+2,Kannur,Baiju,K Sudhakaran (INC),5400
+2,Kannur,Ranish,P K Sreemathi (CPI(M)),3000
+2,Kannur,Shanavas,P K Sreemathi (CPI(M)),5000
+2,Kannur,SKB,P K Sreemathi (CPI(M)),15000
+2,Kannur,Shibu,P K Sreemathi (CPI(M)),5000
+2,Kannur,Shiju,P K Sreemathi (CPI(M)),10000
+2,Kannur,Becker,P K Sreemathi (CPI(M)),8000
+2,Kannur,Swami,P K Sreemathi (CPI(M)),10
+3,Vadakara,Baiju,K Muraleedharan (INC),7500
+3,Vadakara,Ranish,P Jayarajan (CPI(M)),12000
+3,Vadakara,Shanavas,P Jayarajan (CPI(M)),40000
+3,Vadakara,SKB,K Muraleedharan (INC),1000
+3,Vadakara,Shibu,P Jayarajan (CPI(M)),25000
+3,Vadakara,Shiju,P Jayarajan (CPI(M)),40000
+3,Vadakara,Becker,P Jayarajan (CPI(M)),6000
+3,Vadakara,Swami,P Jayarajan (CPI(M)),10
+4,Wayanad,Baiju,Rahul Gandhi (INC),130000
+4,Wayanad,Ranish,Rahul Gandhi (INC),110000
+4,Wayanad,Shanavas,Rahul Gandhi (INC),90000
+4,Wayanad,SKB,Rahul Gandhi (INC),150000
+4,Wayanad,Shibu,Rahul Gandhi (INC),100000
+4,Wayanad,Shiju,Rahul Gandhi (INC),90000
+4,Wayanad,Becker,Rahul Gandhi (INC),150000
+4,Wayanad,Swami,Rahul Gandhi (INC),12
+5,Kozhikode,Baiju,A Pradeep Kumar (CPI(M)),15000
+5,Kozhikode,Ranish,A Pradeep Kumar (CPI(M)),5000
+5,Kozhikode,Shanavas,A Pradeep Kumar (CPI(M)),5000
+5,Kozhikode,SKB,M K Raghavan (INC),150000
+5,Kozhikode,Shibu,A Pradeep Kumar (CPI(M)),2000
+5,Kozhikode,Shiju,A Pradeep Kumar (CPI(M)),30000
+5,Kozhikode,Becker,M K Raghavan (INC),5000
+5,Kozhikode,Swami,A Pradeep Kumar (CPI(M)),2
+6,Malappuram,Baiju,P. K. Kunhalikutty (IUML),75000
+6,Malappuram,Ranish,P. K. Kunhalikutty (IUML),50000
+6,Malappuram,Shanavas,P. K. Kunhalikutty (IUML),80000
+6,Malappuram,SKB,P. K. Kunhalikutty (IUML),10000
+6,Malappuram,Shibu,P. K. Kunhalikutty (IUML),80000
+6,Malappuram,Shiju,P. K. Kunhalikutty (IUML),70000
+6,Malappuram,Becker,P. K. Kunhalikutty (IUML),100000
+6,Malappuram,Swami,VP Sanu (CPI(M)),1
+7,Ponnani,Baiju,E. T. Muhammed Basheer (IUML),65000
+7,Ponnani,Ranish,P V Anwar (CPI(M)),7000
+7,Ponnani,Shanavas,E. T. Muhammed Basheer (IUML),20000
+7,Ponnani,SKB,E. T. Muhammed Basheer (IUML),12000
+7,Ponnani,Shibu,E. T. Muhammed Basheer (IUML),750000
+7,Ponnani,Shiju,P V Anwar (CPI(M)),15000
+7,Ponnani,Becker,E. T. Muhammed Basheer (IUML),10000
+7,Ponnani,Swami,P V Anwar (CPI(M)),2
+8,Palakkad,Baiju,M.B. Rajesh (CPI(M)),30000
+8,Palakkad,Ranish,M.B. Rajesh (CPI(M)),25000
+8,Palakkad,Shanavas,M.B. Rajesh (CPI(M)),30000
+8,Palakkad,SKB,M.B. Rajesh (CPI(M)),1000
+8,Palakkad,Shibu,M.B. Rajesh (CPI(M)),35000
+8,Palakkad,Shiju,M.B. Rajesh (CPI(M)),60000
+8,Palakkad,Becker,M.B. Rajesh (CPI(M)),110000
+8,Palakkad,Swami,M.B. Rajesh (CPI(M)),3
+9,Alathur,Baiju,P.K. Biju (CPI(M)),45000
+9,Alathur,Ranish,P.K. Biju (CPI(M)),8000
+9,Alathur,Shanavas,P.K. Biju (CPI(M)),5000
+9,Alathur,SKB,P.K. Biju (CPI(M)),1000
+9,Alathur,Shibu,Remya Haridas (INC),1000
+9,Alathur,Shiju,P.K. Biju (CPI(M)),20000
+9,Alathur,Becker,P.K. Biju (CPI(M)),30000
+9,Alathur,Swami,P.K. Biju (CPI(M)),2
+10,Thrissur,Baiju,Rajaji Mathew Thomas (CPI(M)),15000
+10,Thrissur,Ranish,Rajaji Mathew Thomas (CPI(M)),5000
+10,Thrissur,Shanavas,T N Prathapan (INC),3000
+10,Thrissur,SKB,T N Prathapan (INC),1250
+10,Thrissur,Shibu,T N Prathapan (INC),15000
+10,Thrissur,Shiju,Rajaji Mathew Thomas (CPI(M)),20000
+10,Thrissur,Becker,Rajaji Mathew Thomas (CPI(M)),12000
+10,Thrissur,Swami,Rajaji Mathew Thomas (CPI(M)),2
+11,Chalakudy,Baiju,Innocent (CPI(M)),45000
+11,Chalakudy,Ranish,Innocent (CPI(M)),10000
+11,Chalakudy,Shanavas,Innocent (CPI(M)),5000
+11,Chalakudy,SKB,Innocent (CPI(M)),1000
+11,Chalakudy,Shibu,Benny Bahanan (INC),8000
+11,Chalakudy,Shiju,Innocent (CPI(M)),30000
+11,Chalakudy,Becker,Innocent (CPI(M)),15000
+11,Chalakudy,Swami,Innocent (CPI(M)),4
+12,Ernakulam,Baiju,Hibi Eden (INC),12000
+12,Ernakulam,Ranish,Hibi Eden (INC),30000
+12,Ernakulam,Shanavas,P. Rajeev (CPI(M)),5000
+12,Ernakulam,SKB,Hibi Eden (INC),1367
+12,Ernakulam,Shibu,Hibi Eden (INC),40000
+12,Ernakulam,Shiju,Hibi Eden (INC),40000
+12,Ernakulam,Becker,Hibi Eden (INC),60000
+12,Ernakulam,Swami,P. Rajeev (CPI(M)),4
+13,Idukki,Baiju,Joice George (CPI(M)),20000
+13,Idukki,Ranish,Joice George (CPI(M)),4000
+13,Idukki,Shanavas,Joice George (CPI(M)),6000
+13,Idukki,SKB,Dean Kuriakose (INC),13000
+13,Idukki,Shibu,Dean Kuriakose (INC),10000
+13,Idukki,Shiju,Dean Kuriakose (INC),40000
+13,Idukki,Becker,Dean Kuriakose (INC),10000
+13,Idukki,Swami,Joice George (CPI(M)),3
+14,Kottayam,Baiju,Thomas Chazhikkadan (INC),35000
+14,Kottayam,Ranish,Thomas Chazhikkadan (INC),40000
+14,Kottayam,Shanavas,Thomas Chazhikkadan (INC),15000
+14,Kottayam,SKB,V.N. Vasavan (CPI(M)),10000
+14,Kottayam,Shibu,Thomas Chazhikkadan (INC),5000
+14,Kottayam,Shiju,Thomas Chazhikkadan (INC),70000
+14,Kottayam,Becker,Thomas Chazhikkadan (INC),60000
+14,Kottayam,Swami,V.N. Vasavan (CPI(M)),5
+15,Alappuzha,Baiju,Shanimol Usman (INC),25000
+15,Alappuzha,Ranish,A.M. Arif (CPI(M)),7000
+15,Alappuzha,Shanavas,A.M. Arif (CPI(M)),40000
+15,Alappuzha,SKB,A.M. Arif (CPI(M)),20000
+15,Alappuzha,Shibu,A.M. Arif (CPI(M)),25000
+15,Alappuzha,Shiju,A.M. Arif (CPI(M)),70000
+15,Alappuzha,Becker,A.M. Arif (CPI(M)),55000
+15,Alappuzha,Swami,A.M. Arif (CPI(M)),6
+16,Mavelikkara,Baiju,Kodikunnil Suresh (INC),30000
+16,Mavelikkara,Ranish,Kodikunnil Suresh (INC),5000
+16,Mavelikkara,Shanavas,Kodikunnil Suresh (INC),40000
+16,Mavelikkara,SKB,Kodikunnil Suresh (INC),20000
+16,Mavelikkara,Shibu,Kodikunnil Suresh (INC),50000
+16,Mavelikkara,Shiju,Chittayam Gopakumar (CPI(M)),15000
+16,Mavelikkara,Becker,Kodikunnil Suresh (INC),30000
+16,Mavelikkara,Swami,Chittayam Gopakumar (CPI(M)),2
+17,Pathanamthitta,Baiju,Veena George (CPI(M)),23000
+17,Pathanamthitta,Ranish,Anto Antony (INC),2000
+17,Pathanamthitta,Shanavas,Veena George (CPI(M)),3000
+17,Pathanamthitta,SKB,Veena George (CPI(M)),10000
+17,Pathanamthitta,Shibu,Anto Antony (INC),3000
+17,Pathanamthitta,Shiju,Veena George (CPI(M)),5000
+17,Pathanamthitta,Becker,Veena George (CPI(M)),10000
+17,Pathanamthitta,Swami,Veena George (CPI(M)),15
+18,Kollam,Baiju,NK Premachandran (RSP),15000
+18,Kollam,Ranish,K.N. Balagopal (CPI(M)),4000
+18,Kollam,Shanavas,K.N. Balagopal (CPI(M)),3000
+18,Kollam,SKB,NK Premachandran (RSP),20000
+18,Kollam,Shibu,NK Premachandran (RSP),20000
+18,Kollam,Shiju,K.N. Balagopal (CPI(M)),25000
+18,Kollam,Becker,NK Premachandran (RSP),30000
+18,Kollam,Swami,K.N. Balagopal (CPI(M)),2
+19,Attingal,Baiju,A. Sampath (CPI(M)),23000
+19,Attingal,Ranish,A. Sampath (CPI(M)),40000
+19,Attingal,Shanavas,A. Sampath (CPI(M)),15000
+19,Attingal,SKB,Adoor Prakash (INC),1000
+19,Attingal,Shibu,A. Sampath (CPI(M)),30000
+19,Attingal,Shiju,A. Sampath (CPI(M)),60000
+19,Attingal,Becker,A. Sampath (CPI(M)),80000
+19,Attingal,Swami,Smt. Shobha Surendran (BJP),8
+20,Thiruvananthapuram,Baiju,Shashi Tharoor (INC),5400
+20,Thiruvananthapuram,Ranish,C. Divakaran (CPI(M)),6000
+20,Thiruvananthapuram,Shanavas,Shashi Tharoor (INC),10000
+20,Thiruvananthapuram,SKB,Kummanam Rajasekharan (BJP),1000
+20,Thiruvananthapuram,Shibu,Kummanam Rajasekharan (BJP),2000
+20,Thiruvananthapuram,Shiju,C. Divakaran (CPI(M)),5000
+20,Thiruvananthapuram,Becker,Shashi Tharoor (INC),5000
+20,Thiruvananthapuram,Swami,C. Divakaran (CPI(M)),12`;
