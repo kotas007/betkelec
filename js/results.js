021d7b0e9a9be6319303c0cfb5c329af07104aad
Anoob Backer <anoob.backer@live.com>
2019-05-24 08:04:13 +0530

    Updating results

--- js/results.js
+++ js/results.js
@@ -1,21 +1,21 @@
 var electionResults=`#,Constituency,Status,Winner-Name,Winner-Margin
-1,Kasaragod,23 May 2019,,
-2,Kannur,23 May 2019,,
-3,Vadakara,23 May 2019,,
-4,Wayanad,23 May 2019,,
-5,Kozhikode,23 May 2019,,
-6,Malappuram,23 May 2019,,
-7,Ponnani,23 May 2019,,
-8,Palakkad,23 May 2019,,
-9,Alathur,23 May 2019,,
-10,Thrissur,23 May 2019,,
-11,Chalakudy,23 May 2019,,
-12,Ernakulam,23 May 2019,,
-13,Idukki,23 May 2019,,
-14,Kottayam,23 May 2019,,
-15,Alappuzha,23 May 2019,,
-16,Mavelikkara,23 May 2019,,
-17,Pathanamthitta,23 May 2019,,
-18,Kollam,23 May 2019,,
-19,Attingal,23 May 2019,,
-20,Thiruvananthapuram,23 May 2019`;
+1,Kasaragod,Complete,Rajamohan Unnithan (INC),40438
+2,Kannur,Complete,K Sudhakaran (INC),94559
+3,Vadakara,Complete,K Muraleedharan (INC),84663
+4,Wayanad,Complete,Rahul Gandhi (INC),431770
+5,Kozhikode,Complete,M K Raghavan (INC),85225
+6,Malappuram,Complete,P. K. Kunhalikutty (IUML),260153
+7,Ponnani,Complete,E. T. Muhammed Basheer (IUML),193273
+8,Palakkad,Complete,V K Sreekantan (INC),11637
+9,Alathur,Complete,Remya Haridas (INC),158968
+10,Thrissur,Complete,T N Prathapan (INC),93633
+11,Chalakudy,Complete,Benny Bahanan (INC),132274
+12,Ernakulam,Complete,Hibi Eden (INC),169153
+13,Idukki,Complete,Dean Kuriakose (INC),171053
+14,Kottayam,Complete,Thomas Chazhikkadan (INC),106259
+15,Alappuzha,Complete,A.M. Arif (CPI(M)),10474
+16,Mavelikkara,Complete,Kodikunnil Suresh (INC),61138
+17,Pathanamthitta,Complete,Anto Antony (INC),44243
+18,Kollam,Complete,NK Premachandran (RSP),148856
+19,Attingal,Complete,Adoor Prakash (INC),38247
+20,Thiruvananthapuram,Complete,Shashi Tharoor (INC),99989`;
